dd3f37ad3480369b4cfbe25451a7af39cf2bdb16
001AJD <ajinkyadhomne.27@gmail.com>
2022-03-18 20:31:18 +0530

    configured winston logger

---
+++ src/helpers/logger.js
@@ -0,0 +1,11 @@
+import winston from 'winston';
+const logConfiguration = {
+	level: 'info',
+	format: winston.format.combine(
+		winston.format.simple(),
+		winston.format.json()
+		),
+	'transports' : [ new winston.transports.Console() ]
+};
+const logger = winston.createLogger(logConfiguration);
+export { logger }
--- src/services/mflixService.js
+++ src/services/mflixService.js
@@ -1,13 +1,18 @@
 import { getRandomMoviesDbFunction } from '../dbFunctions/mflixDbFunctions.js'
 import { getRandomMoviesServiceMapper } from './responseMapper/getRandomMoviesServiceMapper.js';
+import { addLog } from '../helpers/utils.js';
 const getRandomMoviesService = (req, callback) => {
+	const start = process.hrtime.bigint();
+	const functionName = 'mflixService => getRandomMoviesService'
 	getRandomMoviesDbFunction(req,(err,response) => {
 		if(err)
 		{
+			addLog(req, functionName, start, process.hrtime.bigint());
 			callback(err);
 		}
 		else
 		{
+			addLog(req, functionName, start, process.hrtime.bigint());
 			callback(null, getRandomMoviesServiceMapper(response));
 		}
 	});
