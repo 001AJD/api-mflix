a2212f4885334c8832d13afbbac0a190e2575341
001AJD <ajinkyadhomne.27@gmail.com>
2022-03-18 21:14:05 +0530

    added helmet

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from 'express';
 import { router as mflixRouter} from './routes/mflix.js';
 import { createConenction } from './dbconfig/mongoConnection.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import helmet from 'helmet';
 
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(helmet());
 
 // db connection
 createConenction();
