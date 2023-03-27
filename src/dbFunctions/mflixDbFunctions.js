2cf1bf568a4462b44f90e13c85d56bc51e5de616
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2023-03-27 23:53:31 +0530

    updated mongoose package to 7

--- src/dbFunctions/mflixDbFunctions.js
+++ src/dbFunctions/mflixDbFunctions.js
@@ -1,5 +1,5 @@
 import { movies } from '../models/movies.js';
-import { addLog } from '../helpers/utils.js';
+import { addLog, addErrorLog } from '../helpers/utils.js';
 const errorResponse = [
 	{
 		'errCode' : 500
@@ -9,32 +9,33 @@ const errorResponse = [
 	}
 ];
 
-const getRandomMoviesDbFunction = (req,callback) => {
+const getRandomMoviesDbFunction = async (req,callback) => {
 	const start = process.hrtime.bigint();
 	const functionName = 'mflixDbFunctions => getRandomMoviesDbFunction';
 	const query = [
 		{$sample : {size : 5}},
 		{$project : { _id: 0, title: 1, fullplot: 1, year: 1, cast: 1, genres: 1 }}
 	];
-	movies.aggregate(query,(err,dbResult) => {
-		if(err){
-			addLog(req, functionName, start, process.hrtime.bigint());
-			callback(errorResponse[0]);
-		}
-		else
-		{
-			if(dbResult.length > 0)
+	try
+	{
+		const dbResponse = await movies.aggregate(query);
+		addLog(req,functionName,start,process.hrtime.bigint());
+		if(dbResponse.length > 0)
 			{
 				addLog(req, functionName, start, process.hrtime.bigint());
-				callback(null, dbResult);
+				callback(null, dbResponse);
 			}
 			else
 			{
 				addLog(req, functionName, process.hrtime.bigint());
 				callback(errorResponse[1]);
 			}
-		}
-	});
+	}
+	catch (e)
+	{
+		addErrorLog(req,e.toString(),start,process.hrtime.bigint());
+		return { statusCode : 500, message : 'Internal Server Error'};
+	}
 };
 
 export { getRandomMoviesDbFunction };
